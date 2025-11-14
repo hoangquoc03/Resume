2897ed21f3f700104c235cf37861faf2cdaad9d1
lequoc <lequoc150305@quoc150305gmail.com>
2025-11-15 01:24:56 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+const Navbar = () => {
+  const user = { name: "Jion Doe" };
+  const navigate = useNavigate();
+  const logoutUser = () => {
+    navigate("/");
+  };
+  return (
+    <div className="shadow bg-white">
+      <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all">
+        <Link to="/">
+          <img src="/favicon.png" alt="" className="w-[50px]" />
+        </Link>
+        <div className="flex items-center gap-4 text-sm">
+          <p className="max-sm:hidden">Hi, {user?.name}</p>
+          <button
+            onClick={logoutUser}
+            className="bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all"
+          >
+            logout
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/home/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-indigo-600 text-center bg-gradient-to-r from-[#0428f1] to-[#FDFEFF]">
+      <p>
+        <span className="px-3 py-1 rounded-lg text-white text-indigo-600 mr-2">
+          New
+        </span>
+        AI Feature Added
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
