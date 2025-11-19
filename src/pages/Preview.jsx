48588ee0d1f3699ba851ac9ad36ceab70b733993
lequoc <lequoc150305@quoc150305gmail.com>
2025-11-19 15:06:42 +0700

    xcvx

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import ResumePreview from "../components/ResumePreview";
+import { ArrowLeftIcon, Loader } from "lucide-react";
 
 const Preview = () => {
-  return <div>Preview page</div>;
+  const { resumeId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [resumeData, setResumeData] = useState(null);
+
+  const loadResume = async () => {
+    const found =
+      dummyResumeData.find((resume) => resume._id === resumeId) || null;
+
+    setResumeData(found);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadResume();
+  }, []);
+
+  return resumeData ? (
+    <div className="bg-slate-100">
+      <div className="max-w-3xl mx-auto py-10">
+        <ResumePreview
+          data={resumeData}
+          template={resumeData.template}
+          accentColor={resumeData.accent_color}
+          className="py-4 bg-white"
+        />
+      </div>
+    </div>
+  ) : (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <p className="text-center text-6xl text-slate-400 font-medium">
+            Resume not found
+          </p>
+          <a
+            href="/"
+            className="mt-6 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-indigo-500 flex items-center transition-colors"
+          >
+            <ArrowLeftIcon className=" mr size-4" />
+            Go to home page
+          </a>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Preview;
--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -14,6 +14,7 @@ import {
   Share2Icon,
   EyeIcon,
   EyeOffIcon,
+  DownloadIcon,
 } from "lucide-react";
 import PersonalInfoForm from "../components/PersonalInfoForm";
 import ResumePreview from "../components/ResumePreview";
@@ -61,6 +62,21 @@ const ResumeBuilder = () => {
   useEffect(() => {
     loadExistingResume();
   }, []);
+  const changeResumeVisibility = async () => {
+    setResumeData({ ...resumeData, public: !resumeData.public });
+  };
+  const handleShare = () => {
+    const frontendUrl = window.location.href.split("/app/")[0];
+    const resumeUrl = frontendUrl + "/view/" + resumeId;
+    if (navigator.share) {
+      navigator.share({ url: resumeUrl, text: "My Resume" });
+    } else {
+      alert("Share not supported on this browser.");
+    }
+  };
+  const downloadResume = () => {
+    window.print();
+  };
   return (
     <div>
       <div className="max-w-7xl mx-auto px-4 py-6">
@@ -222,17 +238,29 @@ const ResumeBuilder = () => {
               {/* buttons */}
               <div className="absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2">
                 {resumeData.public && (
-                  <button className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors">
-                    <Share2Icon className="size-4" />
+                  <button
+                    onClick={handleShare}
+                    className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors"
+                  >
+                    <Share2Icon className="size-4" /> Share
                   </button>
                 )}
-                <button className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors">
+                <button
+                  onClick={changeResumeVisibility}
+                  className="flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors"
+                >
                   {resumeData.public ? (
                     <EyeIcon className="size-4" />
                   ) : (
                     <EyeOffIcon className="size-4" />
                   )}
                   {resumeData.public ? "Public" : "Private"}
+                </button>
+                <button
+                  onClick={downloadResume}
+                  className="flex items-center p-2 px-6 py-2 gap-2 text-xs bg-gradient-to-br from-indigo-100 to-indigo-200 text-indigo-600 ring-indigo-300 rounded-lg hover:ring transition-colors"
+                >
+                  <DownloadIcon /> Dowload
                 </button>
               </div>
             </div>
